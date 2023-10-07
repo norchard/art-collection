2633cae7a04fc9089e0edc4adfe95a9deb369776
norchard <norchard24@gmail.com>
2023-10-07 18:38:48 -0500

    Prep directory to deploy to Heroku

--- server/index.mjs
+++ server/index.mjs
@@ -34,5 +34,15 @@ app.use((err, _req, res, next) => {
   });
 });
 
+const path = require("path");
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
+
 // start the express server
 app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
